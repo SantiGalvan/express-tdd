e03feb3f8ee6b6c4e4ef953fb74f1c931489b7fa
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-30 18:05:25 +0200

    refacturing and added sixth bonus test

--- test/model.test.js
+++ test/model.test.js
@@ -1,18 +1,17 @@
 const { test, expect } = require("@jest/globals");
 const posts = require('../database/db.json');
+const fs = require('fs');
 
 // Class
 class Model {
-    constructor(fileNameJson) {
-        if (!fileNameJson) {
-            throw new Error('Passare il file JSON');
-        }
 
-        this.file = fileNameJson;
+    constructor(nameFile) {
+        this.nameFile = `${nameFile}` + '.json';
     }
 
     read() {
-        return this.file;
+        const file = fs.readFileSync(`./database/${this.nameFile}`, 'utf8');
+        return JSON.parse(file);
     }
 
     add() {
@@ -22,26 +21,35 @@ class Model {
 
 // 1- Model dovrebbe essere una classe
 test('Model dovrebbe essere una classe', () => {
-    const model = new Model(posts);
+    const model = new Model('db');
 
     expect(model).toBeInstanceOf(Model);
 });
 
 // 2- L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza
-test('- L\'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell\'istanza', () => {
-    expect(() => new Model()).toThrow();
+test("L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza", () => {
+    const model = new Model('db');
+    expect(model.nameFile).toBe('db.json');
 });
 
 // 3- L'istanza di model dovrebbe avere il metodo read
 test('L\'istanza di model dovrebbe avere il metodo read', () => {
-    const model = new Model(posts);
+    const model = new Model('db');
 
-    expect(model.read()).toBeDefined();
+    expect(model.read).toBeInstanceOf(Function);
 });
 
 // 4- L'istanza di model dovrebbe avere il metodo add
 test('L\'istanza di model dovrebbe avere il metodo add', () => {
-    const model = new Model(posts);
+    const model = new Model('db');
 
-    expect(model.add()).toBeDefined();
+    expect(model.add).toBeInstanceOf(Function);
+});
+
+// 5- read dovrebbe ritornare un array
+test('read dovrebbe ritornare un array', () => {
+    const model = new Model('db');
+    const arrayFile = model.read()
+
+    expect(Array.isArray(arrayFile)).toBe(true);
 });
