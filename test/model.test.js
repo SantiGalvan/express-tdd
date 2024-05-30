fdf28ac41eb9171510c676e78a5677dd743b5871
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-30 17:13:48 +0200

    added second bonus test

--- test/model.test.js
+++ test/model.test.js
@@ -1,10 +1,24 @@
 const { test, expect } = require("@jest/globals");
+const posts = require('../database/db.json');
 
 // Class
-class Model { }
+class Model {
+    constructor(fileNameJson) {
+        if (!fileNameJson) {
+            throw new Error('Passare il file JSON');
+        }
+
+        this.file = fileNameJson;
+    }
+}
 
 // 1- Model dovrebbe essere una classe
 test('Model dovrebbe essere una classe', () => {
-    const model = new Model();
+    const model = new Model(posts);
     expect(model).toBeInstanceOf(Model);
+});
+
+// 2- L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza
+test('- L\'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell\'istanza', () => {
+    expect(() => new Model()).toThrow();
 });
