230d276596384b5da8971c1ec77498e2dd366d16
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-30 15:02:55 +0200

    added second test

--- test/slug.test.js
+++ test/slug.test.js
@@ -3,10 +3,15 @@ const posts = require("../database/db.json");
 
 // Funzione da testare
 const createSlug = (title, list) => {
-    return title;
+    return title.toLowerCase();
 }
 
 // 1- createSlug dovrebbe ritornare una stringa
 test('createSlug dovrebbe ritornare una stringa', () => {
     expect(typeof createSlug('Pippo', posts)).toBe('string');
+});
+
+// 2- createSlug dovrebbe ritornare una stringa in lowercase
+test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
+    expect(createSlug('Pippo', posts) === createSlug('Pippo', posts).toLowerCase()).toBe(true)
 });
