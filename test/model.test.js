c3587f52cbbc5d0211c41a3a8dcd105d289316c5
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-30 17:24:10 +0200

    added third bonus test

--- test/model.test.js
+++ test/model.test.js
@@ -10,6 +10,10 @@ class Model {
 
         this.file = fileNameJson;
     }
+
+    read() {
+        return this.file;
+    }
 }
 
 // 1- Model dovrebbe essere una classe
@@ -21,4 +25,11 @@ test('Model dovrebbe essere una classe', () => {
 // 2- L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza
 test('- L\'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell\'istanza', () => {
     expect(() => new Model()).toThrow();
+});
+
+// 3- L'istanza di model dovrebbe avere il metodo read
+test('L\'istanza di model dovrebbe avere il metodo read', () => {
+    const model = new Model(posts);
+
+    expect(model.read()).toBeDefined();
 });
